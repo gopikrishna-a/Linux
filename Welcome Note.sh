f5271fddc8102c71f3b8a94891b1d903dccf6623
Gopikrishna <a.gopikrishna01@gmail.com>
2017-06-07 18:08:38 +0530

    Create Welcome Note.sh

---
+++ Welcome Note.sh
@@ -0,0 +1,13 @@
+# .bash_profile
+
+# Get the aliases and functions
+if [ -f ~/.bashrc ]; then
+        . ~/.bashrc
+fi
+
+# User specific environment and startup programs
+
+PATH=$PATH:$HOME/bin
+source /import/tools/qa/bin/qaenv
+echo "You have to trust in something - your gut, destiny, life, karma, whatever. This approach has never let me down - STEVE JOBS  "
+export PATH
