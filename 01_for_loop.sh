60ad324a517c94c8ba00529066ae9d93bef17017
Gopikrishna <a.gopikrishna01@gmail.com>
2017-08-09 14:40:00 +0530

    Update 01_for_loop.sh

--- 01_for_loop.sh
+++ 01_for_loop.sh
@@ -3,3 +3,18 @@ for i in $(seq 99)
 do
    echo $i
 done
+
+#BASH:printing numbers from 1 to 99
+for ((a = 1 ; a < 100 ; a++))
+do
+echo $a
+done
+
+
+#BASH:printing numbers from 1 to 99 wih a differance of 2
+for ((a = 1 ; a < 100 ; a+=2))
+do
+echo $a
+done
+
+
