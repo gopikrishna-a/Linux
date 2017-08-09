e5b0fc49e58a6e207d8cb3ce572e738398d9a1ec
Gopikrishna <a.gopikrishna01@gmail.com>
2017-08-09 15:04:48 +0530

    Create 03_arithmetic_operations.sh

---
+++ 03_arithmetic_operations.sh
@@ -0,0 +1,17 @@
+read x
+read y
+
+#Addition
+echo $(expr $x + $y)
+
+#Subtraction
+echo $(expr $x - $y)
+
+#Multiplication
+echo $(expr $x \* $y)
+
+#Division
+echo $(expr $x / $y)
+
+#Mdulo
+echo $(expr $x % $y)
